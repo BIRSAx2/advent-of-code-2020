ae4d423d7e53226dc12ba5b4ee4955f4f03f4613
BIRSAx2 <mouhieddinesabir@gmail.com>
2022-08-12 15:08:14 +0000

    Added solutions to day 01

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,39 @@
 defmodule AdventOfCode.Day01 do
+  defp parse(args) do
+    args
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.into(MapSet.new())
+  end
+
+  def find_sum(numbers, sum) do
+    numbers
+    |> Enum.find(&MapSet.member?(numbers, sum - &1))
+    |> case do
+      nil -> nil
+      number -> {number, sum - number}
+    end
+  end
+
   def part1(args) do
+    numbers = parse(args)
+
+    {a, b} = find_sum(numbers, 2020)
+    a * b
+  end
+
+  defp find_three_sum(numbers, sum) do
+    a = Enum.find(numbers, &find_sum(numbers, sum - &1))
+    
+    {b, c} = find_sum(numbers, sum - a)
+    {a, b, c}
   end
 
   def part2(args) do
+    numbers = parse(args)
+
+    {a, b, c} = find_three_sum(numbers, 2020)
+
+    a * b * c
   end
 end
