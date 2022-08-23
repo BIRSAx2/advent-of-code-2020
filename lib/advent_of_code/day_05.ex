0f82f27c055156df188d647b69f89eac661f73bd
BIRSAx2 <mouhieddinesabir@gmail.com>
2022-08-23 16:04:59 +0000

    Improved Part 1

--- lib/advent_of_code/day_05.ex
+++ lib/advent_of_code/day_05.ex
@@ -3,65 +3,43 @@ defmodule AdventOfCode.Day05 do
     String.split(args, "\n", trim: true)
   end
 
-  defp take_upper_half(lower..upper) do
-    lower..(floor((upper + lower + 1) / 2) - 1)
+  def get_seat_id(boarding_pass) do
+    boarding_pass
+    |> String.graphemes()
+    |> Enum.map(&char_to_digit/1)
+    |> Enum.reverse()
+    |> Enum.with_index()
+    |> Enum.reduce(0, fn {digit, exp}, acc -> acc + Bitwise.bsl(digit, exp) end)
   end
 
-  defp take_lower_half(lower..upper) do
-    floor((upper + lower + 1) / 2)..upper
-  end
-
-  defp get_seat_id(seat) do
-    {row.._, col.._} =
-      seat
-      |> String.split("", trim: true)
-      |> Enum.reduce({0..127, 0..7}, fn
-        "F", {rows, cols} ->
-          {take_upper_half(rows), cols}
-
-        "B", {rows, cols} ->
-          {take_lower_half(rows), cols}
-
-        "L", {rows, cols} ->
-          {rows, take_upper_half(cols)}
-
-        "R", {rows, cols} ->
-          {rows, take_lower_half(cols)}
-      end)
-
-    row * 8 + col
+  def char_to_digit(str) do
+    case str do
+      "B" -> 1
+      "F" -> 0
+      "R" -> 1
+      "L" -> 0
+    end
   end
 
   def part1(args) do
-    seats = parse(args)
+    boarding_passes = parse(args)
 
-    seats
+    boarding_passes
     |> Enum.map(&get_seat_id/1)
     |> Enum.max()
   end
 
   def part2(args) do
-    seats = parse(args)
+    boarding_passes = parse(args)
 
-    seat_ids = Enum.map(seats, &get_seat_id/1)
+    [prev, _next] =
+      boarding_passes
+      |> Enum.sort()
+      |> Enum.map(&get_seat_id/1)
+      |> Enum.chunk_every(2, 1, :discard)
+      |> Enum.filter(fn [prev, next] -> next - prev == 2 end)
+      |> hd()
 
-    [635]
-    |> Enum.reduce_while(MapSet.new(seat_ids), fn el, acc ->
-      cond do
-        MapSet.member?(acc, el - 1) and MapSet.member?(acc, el + 1) ->
-          {:cont, acc}
-
-        not MapSet.member?(acc, el + 1) and
-            MapSet.member?(acc, el + 2) ->
-          {:halt, el + 1}
-
-        not MapSet.member?(acc, el - 1) and
-            MapSet.member?(acc, el - 2) ->
-          {:halt, el - 1}
-
-        true ->
-          {:cont, acc}
-      end
-    end)
+    prev + 1
   end
 end
